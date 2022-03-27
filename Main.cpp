87a69493de52b9925cfd8d83d256becd26244ff0
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-03-27 14:13:14 -0300

    Comment types, escape characters basics

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World\n";
+using namespace std;
+
+int main()
+{
+    // This is really the beginning
+    cout << "Hello World\n";
+    cout << "Hello World again" << endl;
+    cout << "\tHello World, but tabbed";
+    /* Who knows when it's going to end? */
 
     return 0;
 }
